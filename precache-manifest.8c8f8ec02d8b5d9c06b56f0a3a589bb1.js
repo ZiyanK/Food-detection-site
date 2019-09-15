9ef7c69902a5f5b162c3534593f0a0a34d1b710d
ZiyanK <ziyankarmali786@gmail.com>
2019-09-15 17:26:54 +0530

    Updates

---
+++ precache-manifest.8c8f8ec02d8b5d9c06b56f0a3a589bb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cbe5dbaa7bbb78d5f848d030ff72ada",
+    "url": "/Food-detection-site/index.html"
+  },
+  {
+    "revision": "a265a02eb9c714c29bee",
+    "url": "/Food-detection-site/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "ac6af5e23e6bb29091fa",
+    "url": "/Food-detection-site/static/css/main.37da344f.chunk.css"
+  },
+  {
+    "revision": "a265a02eb9c714c29bee",
+    "url": "/Food-detection-site/static/js/2.d0da40f8.chunk.js"
+  },
+  {
+    "revision": "ac6af5e23e6bb29091fa",
+    "url": "/Food-detection-site/static/js/main.80034135.chunk.js"
+  },
+  {
+    "revision": "f027258bbf4d6885b959",
+    "url": "/Food-detection-site/static/js/runtime~main.2b71e01f.js"
+  }
+]);
